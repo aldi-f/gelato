91efb4548cdb95c3162a77008dae46dbabc72d8b
muji06 <k.filopati@gmail.com>
2024-11-27 20:49:59 +0100

    download first video on twitter post if it is a playlist

--- app/websites/twitter.py
+++ app/websites/twitter.py
@@ -16,6 +16,7 @@ class Twitter(Base):
         'no_warnings': True,
         'geo_bypass': True,
         "overwrites": True,
+        "playlist_items": '1',
     }
     convert_to_mp4 = False
 
@@ -25,7 +26,25 @@ class Twitter(Base):
         with YoutubeDL(self.yt_params) as ydl:
             info = ydl.extract_info(self.download_url, download=False) or {}
 
+        if "entries" in info:
+            return info["entries"][0].get("filesize",0) or info["entries"][0].get("filesize_approx",0)
         return info.get("filesize",0) or info.get("filesize_approx",0)    
+    
+    @property
+    def title(self) -> str:
+        """
+        Title of the video
+        """
+        with YoutubeDL(self.yt_params) as ydl:
+            info = ydl.extract_info(self.download_url, download=False) or {}
+        
+        if info.get("entries", []):
+            text = info["entries"][0].get("title", "")
+        else: 
+            text = info.get("title", "")
+
+        return "\n`" + text + "`"
+
 
     def download_video(self):
         with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
