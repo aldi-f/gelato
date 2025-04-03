e8199ebfd713c7f9901111d01167effc55291f0f
muji06 <k.filopati@gmail.com>
2025-04-03 14:35:05 +0200

    remove software mp4 convert from 9gag

--- app/websites/nine_gag.py
+++ app/websites/nine_gag.py
@@ -45,33 +45,3 @@ class NineGAG(Base):
             foo.download([self.download_url])
 
 
-    async def convert_video(self):
-        input_file = self.output_path[-1]
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
-            output_name = temp_file.name
-            self.output_path.append(output_name)
-
-        try:
-            cmd = [
-                'ffmpeg',
-                '-i', input_file,
-                '-vcodec', self._ffmpeg_codec,
-                '-y',
-                output_name
-            ]
-
-            process = await asyncio.create_subprocess_exec(
-                *cmd,
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE
-            )
-
-            stdout, stderr = await process.communicate()
-
-            if process.returncode != 0:
-                print(f"FFmpeg error occurred: {stderr.decode()}")
-                raise Exception('FFmpeg failed', stderr.decode())
-
-        except Exception as e:
-            print(f"Error during conversion: {str(e)}")
-            raise
