6162214380c7156a20321475c73729084743a382
muji06 <k.filopati@gmail.com>
2025-05-05 01:23:45 +0200

    use the same event loop

--- app/websites/instagram.py
+++ app/websites/instagram.py
@@ -12,58 +12,63 @@ PLAYWRIGHT_HOST = os.getenv("PLAYWRIGHT_HOST", "ws://127.0.0.1:3000/")
 
 logger = logging.getLogger(__name__)
 
-def find_reel_id(url:str):
-    # Instagram share link is different from actual video
-    # Redirect fixes it
-    # So catch this to make sure we are pulling the correct one
-    real_url = requests.head(url, allow_redirects=True).url
-    pattern = r'https?://(?:www\.)?instagram\.com/(?:reel|p)/([^/?]+)'
-    match = re.search(pattern, real_url)
-    if match:
-        return match.group(1)
+
     
-    
-async def get_reel_video_url(url:str):
-    async with async_playwright() as p:
-        browser = await p.chromium.connect(ws_endpoint=PLAYWRIGHT_HOST)
-        context = await browser.new_context()
-        page = await context.new_page()
-
-        results = []
-
-        async def handle_request(request):
-            if request.url == "https://www.instagram.com/graphql/query" and request.resource_type == "xhr" :
-                response = await request.response()
-                data = await response.json()
-                results.append(data)
-
-        page.on("request", handle_request)
-
-        await page.goto(url)
-
-        await page.wait_for_load_state("networkidle")
-
-        await context.close()
-        await browser.close()
-
-    if len(results) == 0:
-        raise VideoNotFound("No video found")
-    
-    data = None
-    for result in results:
-        if not result["data"].get("xdt_shortcode_media"):
-            continue
-        data = result["data"]["xdt_shortcode_media"]["video_url"]
-    if not data:
-        raise VideoNotFound("No video found")
-    return data
-
 
 class Instagram(Base):
+    _download_url = None
+
+    def find_reel_id(self):
+        # Instagram share link is different from actual video
+        # Redirect fixes it
+        # So catch this to make sure we are pulling the correct one
+        real_url = requests.head(self.url, allow_redirects=True).url
+        pattern = r'https?://(?:www\.)?instagram\.com/(?:reel|p)/([^/?]+)'
+        match = re.search(pattern, real_url)
+        if match:
+            return match.group(1)
+        
+    async def get_download_url(self):
+        async with async_playwright() as p:
+            browser = await p.chromium.connect(ws_endpoint=PLAYWRIGHT_HOST)
+            context = await browser.new_context()
+            page = await context.new_page()
+
+            results = []
+
+            async def handle_request(request):
+                if request.url == "https://www.instagram.com/graphql/query" and request.resource_type == "xhr" :
+                    response = await request.response()
+                    data = await response.json()
+                    results.append(data)
+
+            page.on("request", handle_request)
+
+            await page.goto(self.url)
+
+            await page.wait_for_load_state("networkidle")
+
+            await context.close()
+            await browser.close()
+
+        if len(results) == 0:
+            raise VideoNotFound("No video found")
+        
+        data = None
+        for result in results:
+            if not result["data"].get("xdt_shortcode_media"):
+                continue
+            data = result["data"]["xdt_shortcode_media"]["video_url"]
+        if not data:
+            raise VideoNotFound("No video found")
+        return data
 
     @property
     def download_url(self):
-        return asyncio.run(get_reel_video_url(self.url))
+        if self._download_url is None:
+            loop = asyncio.get_running_loop()
+            self._download_url = loop.create_task(self.get_download_url()).result()
+        return self._download_url
 
     def download_video(self):
         with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
